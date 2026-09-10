0dfe8c23f7b1001838b96799c3e46dbc82586e3b
keany-albertini <71875866+keany-albertini@users.noreply.github.com>
2026-09-10 22:06:06 +0200

    Connect app to Supabase

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL='https://blxdvnkhpxstcpkzrxau.supabase.co';
+const SUPABASE_KEY='sb_publishable_4HY6bloZVgUuZ0hEBOxVeQ_i6ls70Na';
+window.sb=window.supabase.createClient(SUPABASE_URL,SUPABASE_KEY);
