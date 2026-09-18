55c1ed88a12f9924685cad5a451c8fb78ed9dc8b
keany-albertini <71875866+keany-albertini@users.noreply.github.com>
2026-09-18 13:25:52 +0200

    Refresh modules for construction update

--- js/world.js
+++ js/world.js
@@ -1,4 +1,4 @@
-import { CHUNK_SIZE, state, hashRand, distance } from "./data.js?v=8";
+import { CHUNK_SIZE, state, hashRand, distance } from "./data.js?v=9";
 
 function chunkKey(cx, cy) { return cx + ":" + cy; }
 function objectId(cx, cy, kind, i) { return cx + ":" + cy + ":" + kind + ":" + i; }
