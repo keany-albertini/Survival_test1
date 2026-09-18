ec00c829836e9e68e311d7f5a584cb6e6662a5fb
keany-albertini <71875866+keany-albertini@users.noreply.github.com>
2026-09-18 13:25:59 +0200

    Refresh modules for construction update

--- js/survival.js
+++ js/survival.js
@@ -1,4 +1,4 @@
-import { state, clamp } from "./data.js?v=8";
+import { state, clamp } from "./data.js?v=9";
 
 export function updateSurvival(dt, moving, sprinting) {
   state.dayProgress += dt / 300;
