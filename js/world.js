5c4c5a288bbe21b83d92b5288ba3896547e562c6
keany-albertini <71875866+keany-albertini@users.noreply.github.com>
2026-09-18 15:59:26 +0200

    Sync modules to respawn V13

--- js/world.js
+++ js/world.js
@@ -1,4 +1,4 @@
-import { CHUNK_SIZE, state, hashRand, distance } from "./data.js?v=12";
+import { CHUNK_SIZE, state, hashRand, distance } from "./data.js?v=13";
 
 function chunkKey(cx, cy) { return cx + ":" + cy; }
 function objectId(cx, cy, kind, i) { return cx + ":" + cy + ":" + kind + ":" + i; }
