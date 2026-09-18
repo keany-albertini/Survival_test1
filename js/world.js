b26b82d25d23561fb49c939b0387d47632a84672
keany-albertini <71875866+keany-albertini@users.noreply.github.com>
2026-09-18 15:56:41 +0200

    Sync game modules to skills V12

--- js/world.js
+++ js/world.js
@@ -1,4 +1,4 @@
-import { CHUNK_SIZE, state, hashRand, distance } from "./data.js?v=10";
+import { CHUNK_SIZE, state, hashRand, distance } from "./data.js?v=12";
 
 function chunkKey(cx, cy) { return cx + ":" + cy; }
 function objectId(cx, cy, kind, i) { return cx + ":" + cy + ":" + kind + ":" + i; }
