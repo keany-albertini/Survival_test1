f19ff49d393953b51b0b13f69641c79cf4895f79
keany-albertini <71875866+keany-albertini@users.noreply.github.com>
2026-09-18 13:22:10 +0200

    Refresh survival state module

--- js/survival.js
+++ js/survival.js
@@ -1,4 +1,4 @@
-import { state, clamp } from "./data.js?v=6";
+import { state, clamp } from "./data.js?v=8";
 
 export function updateSurvival(dt, moving, sprinting) {
   state.dayProgress += dt / 300;
