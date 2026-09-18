4ae7aec1572f423e7aa5d5c17de86d988bc5b3bb
keany-albertini <71875866+keany-albertini@users.noreply.github.com>
2026-09-18 17:55:32 +0200

    Sync gameplay modules to V14

--- js/survival.js
+++ js/survival.js
@@ -1,4 +1,4 @@
-import { state, clamp } from "./data.js?v=13";
+import { state, clamp } from "./data.js?v=14";
 
 export function updateSurvival(dt, moving, sprinting) {
   state.dayProgress += dt / 300;
