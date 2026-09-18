64aa03499989ca6db82b2515565585fa6812b6c7
keany-albertini <71875866+keany-albertini@users.noreply.github.com>
2026-09-18 15:59:31 +0200

    Sync modules to respawn V13

--- js/fauna.js
+++ js/fauna.js
@@ -1,7 +1,7 @@
-import { state, distance } from "./data.js?v=12";
-import { getNearbyAnimals } from "./world.js?v=12";
-import { addItem } from "./harvest.js?v=12";
-import { saveGame } from "./save.js?v=12";
+import { state, distance } from "./data.js?v=13";
+import { getNearbyAnimals } from "./world.js?v=13";
+import { addItem } from "./harvest.js?v=13";
+import { saveGame } from "./save.js?v=13";
 
 export function hunt(notify) {
   if (state.gameOver) return false;
