998ab205e816df068d625620160e1ba39361758f
keany-albertini <71875866+keany-albertini@users.noreply.github.com>
2026-09-18 13:35:55 +0200

    Refresh modules for harvesting V10

--- js/survival.js
+++ js/survival.js
@@ -1,4 +1,4 @@
-import { state, clamp } from "./data.js?v=9";
+import { state, clamp } from "./data.js?v=10";
 
 export function updateSurvival(dt, moving, sprinting) {
   state.dayProgress += dt / 300;
