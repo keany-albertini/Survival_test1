fbce07bcbfe5222f067550f4efb5b7f07c6841ed
keany-albertini <71875866+keany-albertini@users.noreply.github.com>
2026-09-18 15:56:46 +0200

    Sync game modules to skills V12

--- js/building.js
+++ js/building.js
@@ -1,5 +1,5 @@
-import { state, BUILDING_DATA, ITEM_DATA, distance } from "./data.js?v=10";
-import { saveGame } from "./save.js?v=10";
+import { state, BUILDING_DATA, ITEM_DATA, distance } from "./data.js?v=12";
+import { saveGame } from "./save.js?v=12";
 
 const FOUNDATION_W = 64;
 const FOUNDATION_H = 46;
