34772aac3d8e2521c13153c4f982211d5685b30b
keany-albertini <71875866+keany-albertini@users.noreply.github.com>
2026-09-18 13:14:32 +0200

    Refresh module cache for quickbar update

--- js/world.js
+++ js/world.js
@@ -1,4 +1,4 @@
-import { CHUNK_SIZE, state, hashRand, distance } from "./data.js";
+import { CHUNK_SIZE, state, hashRand, distance } from "./data.js?v=6";
 
 function chunkKey(cx, cy) { return cx + ":" + cy; }
 function objectId(cx, cy, kind, i) { return cx + ":" + cy + ":" + kind + ":" + i; }
