ce3c4655d778370b3437c49e236f5a223c7920eb
keany-albertini <71875866+keany-albertini@users.noreply.github.com>
2026-09-20 14:46:58 +0200

    V17: align module cache version

--- js/combat.js
+++ js/combat.js
@@ -1,7 +1,7 @@
-import { state, distance } from "./data.js?v=14";
-import { getNearbyAnimals } from "./world.js?v=14";
-import { addItem } from "./harvest.js?v=14";
-import { saveGame } from "./save.js?v=14";
+import { state, distance } from "./data.js?v=17";
+import { getNearbyAnimals } from "./world.js?v=17";
+import { addItem } from "./harvest.js?v=17";
+import { saveGame } from "./save.js?v=17";
 
 export function setAim(x, y) {
   const len = Math.hypot(x, y);
