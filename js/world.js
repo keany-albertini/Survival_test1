da5e54206be98dbfb1f2be94b373ab894f9f1e2a
keany-albertini <71875866+keany-albertini@users.noreply.github.com>
2026-09-20 14:46:43 +0200

    V17: align module cache version

--- js/world.js
+++ js/world.js
@@ -1,4 +1,4 @@
-import { CHUNK_SIZE, state, hashRand, distance } from "./data.js?v=14";
+import { CHUNK_SIZE, state, hashRand, distance } from "./data.js?v=17";
 
 function chunkKey(cx, cy) { return cx + ":" + cy; }
 function objectId(cx, cy, kind, i) { return cx + ":" + cy + ":" + kind + ":" + i; }
