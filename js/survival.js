e406224d790fccd7b0ec5bdcd8387a810157441d
keany-albertini <71875866+keany-albertini@users.noreply.github.com>
2026-09-20 15:14:31 +0200

    V19: align shared module state

--- js/survival.js
+++ js/survival.js
@@ -1,4 +1,4 @@
-import { state, clamp } from "./data.js?v=17";
+import { state, clamp } from "./data.js?v=19";
 
 export function updateSurvival(dt, moving, sprinting) {
   state.dayProgress += dt / 300;
