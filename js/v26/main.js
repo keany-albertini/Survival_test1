fbfbb56c8c22702e89fde5af3750813bfdcf702f
keany-albertini <71875866+keany-albertini@users.noreply.github.com>
2026-09-21 07:13:19 +0200

    V26.4: refresh engine entry cache

--- js/v26/main.js
+++ js/v26/main.js
@@ -3,7 +3,7 @@ window.dispatchEvent(new CustomEvent("survival-loading",{
 }));
 
 try{
-  await import("../v20/main.js?v=263");
+  await import("../v20/main.js?v=264");
 }catch(err){
   console.error("V26 base engine failed to start",err);
   window.dispatchEvent(new CustomEvent("survival-loading",{
